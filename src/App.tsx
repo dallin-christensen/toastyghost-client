eb9425cdbd59899327db52362d65c87481e9333a
Dallin Christensen <christensen.dallin@gmail.com>
2023-10-10 21:21:25 -0700

    tailwind init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { RoomProvider } from './context/RoomContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CurrentUserProvider } from './context/UserContext'
-import './app.css'
 import MuiProvider from './styles/MuiProvider'
 import AppBase from './components/AppBase'
 import { SnackbarProvider } from './context/SnackbarContext'
+import './app.css'
 
 const queryClient = new QueryClient()
 
