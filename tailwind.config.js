d723038cb56e89449bbb93165c338bc497c0b163
Dallin Christensen <christensen.dallin@gmail.com>
2023-10-12 22:18:08 -0700

    tailwind-migrate pt 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      background: '#fff',
+      primary: '#673ab7',
+      messageblue: '#09f',
+      // violet: colors.violet,
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
