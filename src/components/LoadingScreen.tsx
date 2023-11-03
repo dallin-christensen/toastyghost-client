9c1424e812d990ec229faf7f7f5cba2e321324eb
Dallin Christensen <christensen.dallin@gmail.com>
2023-11-03 15:07:42 -0700

    loading screen updated to be brutalist design

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,11 +1,14 @@
+import { memo } from 'react'
 import { ImSpinner5 } from 'react-icons/im'
 
 function LoadingScreen() {
   return (
-    <div className="flex w-full h-full items-center justify-center">
-      <ImSpinner5 className="text-6xl text-black animate-spin" />
+    <div className="flex w-full h-screen items-center justify-center">
+      <div className="w-20 h-20 bg-primary rounded-full flex justify-center items-center shadow-brutal border-2 border-black">
+        <ImSpinner5 className="text-6xl text-black animate-spin" />
+      </div>
     </div>
   )
 }
 
-export default LoadingScreen
+export default memo(LoadingScreen)
