ec95cc26a530ea76e1b326b6222ec9c1b840e817
Dallin Christensen <christensen.dallin@gmail.com>
2023-11-04 18:31:23 -0700

    revised loading component

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,12 +1,12 @@
 import { memo } from 'react'
-import { CgSpinnerTwoAlt } from 'react-icons/cg'
+import GhostSVG from '../assets/ghost.svg?react' // eslint-disable-line import/no-unresolved
 
 function LoadingScreen() {
   return (
     <div className=" absolute flex w-full h-screen items-center justify-center bg-white z-50">
       <div className="w-20 h-20 rounded-full flex justify-center items-center shadow-brutal border-2 border-black opacity-100">
-        <div className="flex justify-center items-center w-full h-full rounded-full bg-gradient-to-tl from-primary from-50% to-backgroundsecondary to-50% animate-spin">
-          <CgSpinnerTwoAlt className="text-5xl text-black" />
+        <div className="flex justify-center items-center w-full h-full rounded-full bg-gradient-to-br bg-primary animate-spin [&>svg]:h-12 [&>svg]:w-12 [&>svg]:pb-1 [&>svg]:pr-0">
+          <GhostSVG />
         </div>
       </div>
     </div>
